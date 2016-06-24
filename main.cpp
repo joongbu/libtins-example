b00e2fa23c6e83b6f5fff75c772ded00faaa4613
joongbu <tmdrbs123@naver.com>
2016-06-24 15:16:25 +0900

    first test

--- main.cpp
+++ main.cpp
@@ -31,43 +31,52 @@
 #include <set>
 #include <string>
 #include <tins/tins.h>
-
 using std::set;
 using std::cout;
 using std::endl;
 using std::string;
 using std::runtime_error;
-
 using namespace Tins;
+
+//class Beaconsniffer
+struct AP_info
+{
+    string AP_name[50];
+    string AP_mac[50];
+};
 
 class BeaconSniffer {
 public:
     void run(const string& iface);
 private:
-    typedef Dot11::address_type address_type;
-    typedef set<address_type> ssids_type;
+    typedef Dot11::address_type address_type; //mac address
+    typedef set<address_type> ssids_type; //ssid
 
     bool callback(PDU& pdu);
 
     ssids_type ssids;
 };
 
+
 void BeaconSniffer::run(const std::string& iface) {
     SnifferConfiguration config;
-    config.set_promisc_mode(true);
-    config.set_filter("type mgt subtype beacon");
+    config.set_promisc_mode(true); //?
+    config.set_filter("type mgt subtype beacon"); //
     config.set_rfmon(true);
     Sniffer sniffer(iface, config);
     sniffer.sniff_loop(make_sniffer_handler(this, &BeaconSniffer::callback));
+
 }
 
 bool BeaconSniffer::callback(PDU& pdu) {
     // Get the Dot11 layer
     const Dot11Beacon& beacon = pdu.rfind_pdu<Dot11Beacon>();
+    AP_info *AP;
     // All beacons must have from_ds == to_ds == 0
-    if (!beacon.from_ds() && !beacon.to_ds()) {
+    if (!beacon.from_ds() && !beacon.to_ds())
+    {
         // Get the AP address
-        address_type addr = beacon.addr2();
+        address_type addr = beacon.addr2(); //802.11 header second mac address sniffing
         // Look it up in our set
         ssids_type::iterator it = ssids.find(addr);
         if (it == ssids.end()) {
@@ -77,14 +86,68 @@ bool BeaconSniffer::callback(PDU& pdu) {
                  * a std::runtime_error.
                  */
                 string ssid = beacon.ssid();
+                ssids.insert(addr); //save address
                 // Save it so we don't show it again.
-                ssids.insert(addr);
                 // Display the tuple "address - ssid".
-                cout << addr << " - " << ssid << endl;
+                cout << addr << " - " << ssid << endl; //get AP SSID , MAC
             }
             catch (runtime_error&) {
                 // No ssid, just ignore it.
             }
+        }
+    }
+    return true;
+}
+
+
+class probeSniffer {
+public:
+    void running(const string& iface);
+private:
+    typedef Dot11::address_type address_type; //mac address
+    typedef set<address_type> ssids_type; //ssid
+
+    bool call(PDU& pdu);
+
+    ssids_type ssids;
+};
+
+void probeSniffer::running(const std::string& iface) {
+    SnifferConfiguration config;
+    config.set_promisc_mode(true); //?
+    //config.set_filter("type mgt subtype beacon");
+    config.set_rfmon(true);
+    Sniffer sniffer(iface, config);
+    sniffer.sniff_loop(make_sniffer_handler(this, &probeSniffer::call));
+
+}
+
+bool probeSniffer::call(PDU& pdu) {
+    // Get the Dot11 layer
+    const Dot11ProbeRequest& probe = pdu.rfind_pdu<Dot11ProbeRequest>();
+
+    // Get the AP address
+    address_type addr = probe.addr2(); //802.11 header second mac address sniffing
+
+    // Look it up in our set
+    ssids_type::iterator it = ssids.find(addr);
+    if (it == ssids.end()) {
+
+        // First time we encounter this BSSID.
+        try {
+            /* If no ssid option is set, then Dot11::ssid will throw
+                 * a std::runtime_error.
+                 */
+            string ssid = probe.ssid();
+            // Save it so we don't show it again.
+            ssids.insert(addr); //save address
+            // Display the tuple "address - ssid".
+            cout<<"==================================="<<endl;
+            cout << addr << " - " << ssid << endl; //get AP SSID , MAC
+
+        }
+        catch (runtime_error&) {
+            // No ssid, just ignore it.
         }
     }
     return true;
@@ -97,5 +160,7 @@ int main(int argc, char* argv[]) {
     }
     string interface = argv[1];
     BeaconSniffer sniffer;
-    sniffer.run(interface);
+    probeSniffer  probe;
+    //sniffer.run(interface);
+    probe.running(interface); //find probe request packet
 }

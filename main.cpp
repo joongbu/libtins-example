e2c0acde4d4cdbed65ca1a522aad1e51af9f807d
joongbu <tmdrbs123@naver.com>
2016-06-28 02:44:26 +0900

    time log

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@
 #include <tins/tins.h>
 #include <thread>
 #include <time.h>
+#include <unistd.h>
 using std::cin;
 using std::set;
 using std::cout;
@@ -44,17 +45,87 @@ typedef Dot11::address_type address_type; //mac address
 typedef set<address_type> ssids_type; //ssid
 ssids_type ssids;
 string interface;
+/*=======================time*/
+time_t now;
+
+/*=======================time*/
 struct student
 {
     string name;
     address_type mac;
+    string s_time = "0";
+    string l_time = "0";
+    bool check = false;
 
 };
+int size = 3;
+student stu[3];
+class stu_info
+{
+
+public :
+
+    void save_info();
+    void attendance();
+    void time_log();
+private:
+
+
+};
+void stu_info::save_info()
+{
+    int i ;
+    for(i = 0 ; i < size ; i++)
+    {
+        cout<<"input student name :";
+        cin>>stu[i].name;
+        cout<<"input student mac address :";
+        //cin>>stu.mac;
+        scanf("%s",&stu[i].mac);
+        ssids_type::iterator it = ssids.find(stu[i].mac);
+        if(it == ssids.end()){
+            try{
+                ssids.insert(stu[i].mac);
+                cout<<"save"<<endl;
+            }
+            catch(runtime_error&) {
+                // No ssid, just ignore it.
+            }
+        }
+    }
+}
+
+void stu_info::attendance()
+{
+    int i;
+    for(i = 0 ; i<size ; i++)
+    {
+        if(stu[i].check == true)
+            cout<<"name :"<<stu[i].name<<"time : "<<stu[i].s_time<<"class : attandence"<<endl;
+        else if(stu[i].check == false)
+            cout<<"name :"<<stu[i].name<<"time : "<<stu[i].s_time<<"class : miss a class"<<endl;
+    }
+}
+void stu_info::time_log()
+{
+    time(&now);
+    int i;
+    for(i = 0 ; i < size ; i++)
+    {
+        try{
+            cout<<"name :"<<stu[i].name<<"start time :"<<stu[i].s_time<<"last time :"<<stu[i].l_time<<endl;
+        }
+        catch(runtime_error&) {
+            // No ssid, just ignore it.
+        }
+    }
+}
+
 class probeSniffer {
 public:
     void running(const string& iface);
+
 private:
-    bool timeclear();
     bool call(PDU& pdu);
 };
 void probeSniffer::running(const std::string& iface) {
@@ -65,100 +136,80 @@ void probeSniffer::running(const std::string& iface) {
     sniffer.sniff_loop(make_sniffer_handler(this, &probeSniffer::call));
 }
 bool probeSniffer::call(PDU& pdu) {
+    time(&now);
     // Get the Dot11 layer
     const Dot11ProbeRequest& probe = pdu.rfind_pdu<Dot11ProbeRequest>();
     // Get the AP address
     address_type addr = probe.addr2(); //802.11 header second mac address sniffing
     // Look it up in our set
+    stu_info info;
     ssids_type::iterator it = ssids.find(addr);
+    int i;
     if (it == ssids.end()) {
         // First time we encounter this BSSID.
         try {
             /* If no ssid option is set, then Dot11::ssid will throw
                  * a std::runtime_error.
                  */
-            string ssid = probe.ssid();
             // Save it so we don't show it again.
             ssids.insert(addr); //save address
             // Display the tuple "address - ssid".
-            cout << addr << " - " << ssid << endl; //get AP SSID , MAC
+            //info.time_log();
+            for(i = 0; i<size ; i++)
+            {
+                if(addr == stu[i].mac)
+                {stu[i].check = true;
+                    stu[i].s_time = ctime(&now);
+                }
+                it = ssids.find(stu[i].mac);
+                if(it == ssids.end())
+                    stu[i].l_time = ctime(&now);
+            }
 
         }
         catch (runtime_error&) {
             // No ssid, just ignore it.
         }
-    }
-
-    return true;
-}
-/*
-bool probeSniffer::timeclear()
-{
-    if(sleep(300))
-        true;
-    false;
-}
-*/
-class stu_info
-{
-
-public :
-    int number;
-    stu_info();
-    student info[1000];
-    void saveinfo();
-    void attendence();
-};
-void stu_info::attendence()
-{
-    time_t now;
-    time(&now);
-    int i;
-    for(i  = 0 ; i < number ; i++)
-    {
-        ssids_type::iterator it = ssids.find(info[i].mac);
-        if(it == ssids.end())
-            cout<<info[i].name<<ctime(&now)<<endl;
-        else
-            cout<<info[i].name<<"not attendence"<<endl;
-
+        return true;
     }
 }
-
-void stu_info::saveinfo()
-{
-    int i;
-    cout<<"input number of people :";
-    cin >> number;
-
-    for(i = 0 ; i < number ; i++)
-    {
-        cout<<"input student name :";
-        cin>>info[i].name;
-        cout<<endl;
-        cout<<"input student mac address";
-        scanf("%s",&info[i].mac);
-
-    }
-    attendence();
-
-}
-
-
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         cout << "Usage: " <<* argv << " <interface>" << endl;
         return 1;
     }
-
+    int i;
     // search probe request
     interface = argv[1];
-    cout<<"getting probe request packet.............."<<endl;
+    stu_info student_information;
+    //student_information.save_info();
     std::thread([] {
         probeSniffer  probe;
         probe.running(interface);
     }).detach();
-    stu_info student_information;
-    student_information.saveinfo();
 
+    stu[0].name="한승균";
+    stu[0].mac ="90:00:db:bb:98:c5";
+    stu[1].name="백종열";
+    stu[1].mac ="64:bc:0c:68:e5:71";
+    stu[2].name ="이혜빈";
+    stu[3].mac = "48:59:29:f4:a5:87";
+    for(i=0; i<size ; i++)
+    {
+        ssids_type::iterator it = ssids.find(stu[i].mac);
+        if(it == ssids.end()){
+            try{
+                ssids.insert(stu[i].mac);
+            }
+            catch(runtime_error&) {
+                // No ssid, just ignore it.
+            }
+        }
+    }
+    while(1)
+    {
+        //student_information.attendance();
+        student_information.time_log();
+        sleep(10);
+    }
 }
